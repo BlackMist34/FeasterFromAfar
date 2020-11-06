8e04f82de9d73f267f6b7962161bb19ba04b9b55
Aidan Yerasimides <73114345+BlackMist34@users.noreply.github.com>
2020-11-06 05:51:38 -0500

    Add files via upload

---
+++ ParticleEffects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ParticleEffects : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        Invoke("Hide", 1f);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
