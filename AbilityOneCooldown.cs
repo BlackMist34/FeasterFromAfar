2e9eebb1d58e3fe7fc7a28660a651c6cf7e1a3c5
Aidan Yerasimides <73114345+BlackMist34@users.noreply.github.com>
2020-11-06 05:44:29 -0500

    Add files via upload

---
+++ AbilityOneCooldown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityOneCooldown : MonoBehaviour
+{
+    public GameObject player;
+
+    private Animator anim;
+    private CharacterStats stats;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        stats = player.GetComponent<CharacterStats>();
+    }
+
+    private void Update()
+    {
+        if (stats.GetEnergy() < 150)
+            this.gameObject.GetComponent<Image>().fillAmount = 1;
+        else
+            this.gameObject.GetComponent<Image>().fillAmount = 0;
+    }
+
+    public void Cooldown()
+    {
+        anim.SetBool("AbilityOne", true);
+    }
+}
