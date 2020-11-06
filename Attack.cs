d79cdd29cdd2d6aec50c1fd4d7b79a8bb90f1b40
Aidan Yerasimides <73114345+BlackMist34@users.noreply.github.com>
2020-11-06 05:54:56 -0500

    Add files via upload

---
+++ Attack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack 
+{
+    public readonly float damage;
+    public readonly bool isCritical;
+
+    public Attack(float damage, bool isCritical)
+    {
+        this.damage = damage;
+        this.isCritical = isCritical;
+    }
+
+    public float GetDamage() { return damage; }
+    public bool GetCritical() { return isCritical; }
+}
