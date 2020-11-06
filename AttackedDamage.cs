77d4f86572f93b57ff0729da480e5861ecc243ca
Aidan Yerasimides <73114345+BlackMist34@users.noreply.github.com>
2020-11-06 05:54:29 -0500

    Add files via upload

---
+++ AttackedDamage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class AttackedDamage : MonoBehaviour, IAttackable
+{
+	private CharacterStats stats;
+
+	private void Awake()
+	{
+		stats = GetComponent<CharacterStats>();
+	}
+
+	public void OnAttack(GameObject attacker, Attack attack)
+	{
+		stats.TakeDamage(attack.GetDamage());
+
+		if (stats.GetHealth() <= 0)
+		{
+			//Destroy Object
+			var destructibles = GetComponents(typeof(IDestructible));
+
+			foreach (IDestructible destructible in destructibles)
+			{
+				destructible.OnDestruction(attacker);
+			}
+		}
+	}
+}
+
