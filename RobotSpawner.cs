053e2b42e09aa463882584710246d63fb58f0f83
Aidan Yerasimides <73114345+BlackMist34@users.noreply.github.com>
2020-11-06 05:53:13 -0500

    Add files via upload

---
+++ RobotSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class RobotSpawner : MonoBehaviour
+{
+    private int spawnCount;
+
+    private void Awake()
+    {
+        spawnCount = 0;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 0f, 10f);
+    }
+
+    void Spawn()
+    {
+        spawnCount++;
+
+        if (spawnCount > 8)
+            RobotManager.Instance.RequestLv5Soldier(gameObject);
+        else if (spawnCount > 6 && spawnCount <= 8)
+            RobotManager.Instance.RequestLv4Soldier(gameObject);
+        else if (spawnCount > 4 && spawnCount <= 6)
+            RobotManager.Instance.RequestLv3Soldier(gameObject);
+        else if (spawnCount > 2 && spawnCount <= 4)
+            RobotManager.Instance.RequestLv2Soldier(gameObject);
+        else
+            RobotManager.Instance.RequestLv1Soldier(gameObject);
+    }
+
+    public int GetSpawnCount()
+    {
+        return spawnCount;
+    }
+}
